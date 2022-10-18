168c107c18567df2f2aa51d245b827b12d59002e
alielsayed76 <98778110+alielsayed76@users.noreply.github.com>
2022-10-19 01:30:11 +0200

    Update filters.py

--- driver/filters.py
+++ driver/filters.py
@@ -7,9 +7,7 @@ other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.fo
 other_filters2 = (
     filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 )
-other_filters3 = (
-    filters.private & ~filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
-)
+other_filters3 = filters.group & ~filters.edited & ~filters.private
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, COMMAND_PREFIXES)
